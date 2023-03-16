515a7f16f6f6141ad02fe5581acf365f1115edff
SPRING249 <2106107257@qq.com>
2023-03-16 23:54:02 +0800

    2023/3/16

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 //引入路由
 import router from "@/router";
-
+// 三级联动组件
+import TypeNav from "@/pages/Home/TypeNav"
 Vue.config.productionTip = false
-
+// 第一个参数：全局组件的名字，第二个参数：哪一个组件
+Vue.component(TypeNav.name,TypeNav)
 new Vue({
   render: h => h(App),
   // 注册路由
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,10 +1,17 @@
 <template>
-
+<div>
+<!--  三级联动组件，全局组件不需要引入-->
+  <TypeNav/>
+</div>
 </template>
 
 <script>
+import ListContainer from "@/pages/Home/ListContainer/index.vue";
 export default {
-  name: "index"
+  name: "index",
+  components: {
+    ListContainer
+  }
 }
 </script>
 
