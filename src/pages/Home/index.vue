9aafa8ddd5fd5c4862b78f16f08f3c311c9d15d3
SPRING249 <2106107257@qq.com>
2023-03-17 21:56:26 +0800

    2023/3/17

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -2,15 +2,28 @@
 <div>
 <!--  三级联动组件，全局组件不需要引入-->
   <TypeNav/>
+  <ListContainer></ListContainer>
+  <Recommend></Recommend>
+  <Rank></Rank>
+  <Like></Like>
+  <Floor></Floor>
+  <Floor></Floor>
+  <Brand></Brand>
 </div>
 </template>
 
 <script>
 import ListContainer from "@/pages/Home/ListContainer/index.vue";
+import Recommend from "@/pages/Home/Recommend/index.vue";
+import Rank from "@/pages/Home/Rank/index.vue";
+import Like from "@/pages/Home/Like/index.vue";
+import Floor from "@/pages/Home/Floor/index.vue";
+import Brand from "@/pages/Home/Brand/index.vue";
 export default {
-  name: "index",
+  name: "Home",
   components: {
-    ListContainer
+    Brand,
+    ListContainer,Recommend,Rank,Like,Floor
   }
 }
 </script>
